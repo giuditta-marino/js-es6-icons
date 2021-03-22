40211ea2b3d8cd9830b3896902229d8243f91827
Giuditta <giudittamarino89@gmail.com>
2021-03-22 14:33:35 +0100

    snack 1

---
+++ jsnack_1/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+// Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+const bikes = [
+  {
+    nome: 'Graziella',
+    peso: 2
+  },
+  {
+    nome: 'BMX',
+    peso: 3
+  },
+  {
+    nome: 'Mountain bike',
+    peso: 1
+  },
+  {
+    nome: 'Passeggio',
+    peso: 5
+  },
+  {
+    nome: 'Pieghevole',
+    peso: 4
+  },
+  {
+    nome: 'Elettrica',
+    peso: 2
+  },
+]
+
+  console.log(bikes);
+
+  // Stampare a schermo la bici con peso minore.
+  let biciLeggera= bikes[0];
+
+  bikes.forEach((item, i) => {
+    if (item.peso < biciLeggera.peso) {
+      biciLeggera = item;
+    }
+  });
+
+  console.log(biciLeggera);
+
+
+
+})
